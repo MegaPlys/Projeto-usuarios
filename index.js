20abaf69927f15a87e8c168d844ca52636f4f466
Júlio Rodrigues <megaplaysoficial1@gmail.com>
2023-09-04 11:32:40 -0300

    constructor js

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var user = {};
 
 function addLine(dataUser){
 
+    console.log(dataUser)
+
     var tr = document.createElement("tr")
 
-    tr.innerHTML = `
+    document.getElementById('table-users').innerHTML = `
         <tr>
             <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
@@ -45,7 +47,18 @@ document.getElementById("form-user-create").addEventListener("submit", function(
     
     });
 
-    addLine(user)
+    var objectUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin
+    )
+
+    addLine(objectUser)
 
 });
 
---
+++ models/User.js
@@ -0,0 +1,16 @@
+class User {
+
+    constructor(name, gender, birth, county, email, password, photo, admin){
+
+        this.name = name
+        this.gender = gender
+        this.birth = birth
+        this.county = county
+        this.email = email
+        this.password = password
+        this.photo = photo
+        this.admin = admin
+
+    }
+
+}
