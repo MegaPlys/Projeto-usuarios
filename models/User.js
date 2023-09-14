a3f36a41c9679418bdfee5946d4cfef15a647598
Júlio Rodrigues <megaplaysoficial1@gmail.com>
2023-09-14 12:35:48 -0300

    getters e setters

--- models/User.js
+++ models/User.js
@@ -2,15 +2,60 @@ class User {
 
     constructor(name, gender, birth, county, email, password, photo, admin){
 
-        this.name = name
-        this.gender = gender
-        this.birth = birth
-        this.county = county
-        this.email = email
-        this.password = password
-        this.photo = photo
-        this.admin = admin
+        this._name = name
+        this._gender = gender
+        this._birth = birth
+        this._county = county
+        this._email = email
+        this._password = password
+        this._photo = photo
+        this._admin = admin
+        this._register = new Date()
 
     }
 
+    get register(){
+        return this._register
+
+    }
+
+    get name(){
+        return this._name
+    }
+
+    get gender(){
+        return this._gender
+
+    }
+
+    get birth(){
+
+        return this._birth
+    }
+
+    get county(){
+        return this._county
+
+    }
+
+    get email(){
+        return this._email
+    
+    }
+
+    get password(){
+        return this._password
+    }
+
+    get photo(){
+        return this._photo
+    }
+
+    get admin(){
+        return this._admin
+    }
+        
+    set photo(value){
+        this._photo = value
+    }
 }
